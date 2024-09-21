c94f9d73b81ee6e53c3321e860f7a19c6d6e3a28
AUniverse <avoteamoficial@Gmail.com>
2024-09-21 10:00:11 +0300

    Create wallpapers.c

---
+++ OS/apps/wallpapers/wallpapers.c
@@ -0,0 +1,10 @@
+#include <tomato.h>
+#include <tomato_ui.h>
+
+int main(void)
+{
+    // Initialize wallpapers application
+    tomato_ui_init();
+    // Load wallpapers and display them
+    return 0;
+}
